45c6fc413c645b5ddc94dcee45a5088911739765
Anka_S <anna.k.sidorova@gmail.com>
2022-11-20 16:05:03 +0300

    Task02

---
+++ Homework01/Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿double a;
+double b;
+double c;
+double max;
+
+a = 170;
+b = 20;
+c = 90;
+max = a;
+
+if ( b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.Write("Максимальное число - ");
+Console.Write(max);
