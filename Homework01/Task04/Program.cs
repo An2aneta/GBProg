9b0405e854881707a514b4e845e6e11eb029427e
Anka_S <anna.k.sidorova@gmail.com>
2022-11-20 16:55:58 +0300

    Task04 added

---
+++ Homework01/Task04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+int N;
+int i;
+string line;
+
+N = 21;
+i = 2;
+line = "";
+
+while(i <= N)
+{
+    line = line + i.ToString() + " ";
+    i = i + 2;
+}
+
+Console.WriteLine(line);
