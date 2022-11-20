ae89b7143f294e5f17effa575b17e42bf9d6a486
Anka_S <anna.k.sidorova@gmail.com>
2022-11-20 15:45:29 +0300

    Task01

---
+++ Homework01/Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿double a = 0;
+double b = 0;
+string a1;
+string b1;
+Console.WriteLine("Введите первое число");
+a1 = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+b1 = Console.ReadLine();
+
+// Нужно проверить на пустую строку, либо ввести маску только на цифры
+a = double.Parse(a1);
+b= double.Parse(b1);
+
+if (a > b)
+{
+    Console.WriteLine("первое число больше второго");
+}
+else
+{
+     Console.WriteLine("второе число больше первого");
+}
