b0edf151b907ae80f199dba2c13d12c8bad9be80
Anka_S <anna.k.sidorova@gmail.com>
2022-11-21 20:55:45 +0300

    Задача готова

--- Seminar02/S02Task01/Program.cs
+++ Seminar02/S02Task01/Program.cs
@@ -5,14 +5,23 @@
 // в программу вводится 2 число
 // найти остаток от деления ч1 на ч2
 // проверить если остаток не равен 0 то показать его
-// иначе показать сообщение о кратности
+// иначе показать сообщение о кратнос
 
+Console.WriteLine("start");
 
+int firstNumber = 25;
+int secondNumber = 6;
 
+int remainder = firstNumber % secondNumber;
 
+if (remainder !=0)
+{
 
+    Console.WriteLine(remainder);
+}
 
+else
+{
 
-
-
-Console.WriteLine("Hello, World!");
+    Console.WriteLine("Кратно");
+}
