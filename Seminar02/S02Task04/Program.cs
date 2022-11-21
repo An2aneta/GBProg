d92fefd6aa2464ac833aea3b42693eb59b10f903
Anka_S <anna.k.sidorova@gmail.com>
2022-11-21 22:21:07 +0300

    Задача 04  готова

---
+++ Seminar02/S02Task04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа которая принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+
+// Вводим число
+// ДЕлим число на 7 и 23
+// если делится - то кратно
+// если нет - не кратно
+
+Console.WriteLine("Enter value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+
+int a = value % 7;
+int b = value % 23;
+
+if(a == 0 & b == 0)
+{
+
+    Console.WriteLine("Кратно");
+}
+
+else
+{
+    Console.WriteLine("Не кратно");
+}
