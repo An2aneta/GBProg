ab02593cf6960534ee7a63153dc892fab3c880d0
Anka_S <anna.k.sidorova@gmail.com>
2022-11-21 22:50:02 +0300

    Задача 05  готова

--- Seminar02/S02Task04/Program.cs
+++ Seminar02/S02Task04/Program.cs
@@ -5,14 +5,35 @@
 // если делится - то кратно
 // если нет - не кратно
 
-Console.WriteLine("Enter value: ");
-int value = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value1: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter value2: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+if(value1*value1 == value2) 
+{
+
+    Console.WriteLine("value2 является квадратом value1");
+}
+else 
+
+//Console.WriteLine("Ни одно из чисел не является квадратом другого");
 
 
-int a = value % 7;
-int b = value % 23;
+if(value2*value2 == value1) 
+{
 
-if(a == 0 & b == 0)
+    Console.WriteLine("value1 является квадратом value2");
+}
+else Console.WriteLine("Ни одно из чисел не является квадратом другого");
+
+
+
+// int a = value % 7;
+// int b = value % 23;
+
+/*if(a == 0 & b == 0)
 {
 
     Console.WriteLine("Кратно");
@@ -22,3 +43,4 @@ else
 {
     Console.WriteLine("Не кратно");
 }
+*/
