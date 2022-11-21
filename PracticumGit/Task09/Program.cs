faf131f0365afbe6a7475ac3a5f467959ae599b4
Anka_S <anna.k.sidorova@gmail.com>
2022-11-21 20:48:25 +0300

    Блок-схема задачи готова

---
+++ PracticumGit/Task09/Program.cs
@@ -0,0 +1,25 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+int N;
+
+N = 521;
+
+while(N >= 100)
+{
+    N = N - 100;
+}
+
+if(N < 10)
+{
+    Console.WriteLine(N);
+}
+    
+else
+{
+    while(N >= 10)
+    {
+        N = N - 10;
+    }
+    Console.WriteLine(N);
+}
+
