cf6342200d526fd3d5360b9766fb6b14b49f9fba
Anka_S <anna.k.sidorova@gmail.com>
2022-11-22 19:25:53 +0300

    New task01 and 02 from lection03 added

---
+++ Lection03/L03Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Методы первой группы
+
+/*
+void Method1()
+{
+    Console.WriteLine("Autor ...")
+};
+*/
+
+// Методы четвертой группы
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "aabbcc");
+Console.WriteLine(res);
---
+++ Lection03/L03Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿// вывод таблицы умножения до 10
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <=10; j++)
+    {
+        Console.WriteLine($"{i}*{j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+
+
+
