37786dd17763259500f5d88f3fa6a3d888e0f35a
Anka_S <anna.k.sidorova@gmail.com>
2022-11-23 21:07:04 +0300

    Задача 10 ко второму семинару добавлена

---
+++ Homework02/Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+int secondNumber = (value % 100) / 10;
+
+Console.WriteLine(secondNumber);
