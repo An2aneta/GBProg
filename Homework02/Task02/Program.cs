e8b9e6661dbc4963690e40bb6881a5004a60ad18
Anka_S <anna.k.sidorova@gmail.com>
2022-11-23 23:12:51 +0300

    Добавлены задания по второму дз

---
+++ Homework02/Task02/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+int GetNumber(string text)
+{
+  Console.Write(text);
+  int value = int.Parse(Console.ReadLine());
+  return value;
+}
+
+int GetLehgthNumber(int value)
+{
+    string str = value.ToString();
+    int length = str.Length;
+    return length;
+}
+
+
+int GiveThirdNumber(int value, int length)
+{ 
+    int tempNumber = 0;
+    int tempValue = value;
+    int result = 0;
+    if(length <=2) 
+    {
+        return result;
+    }
+    else
+    {
+        for(int i = 1; i < 4; i++)
+        {
+            tempNumber = (tempValue / Convert.ToInt32((Math.Pow(10, length-i))));
+            tempValue = tempValue - (tempNumber * Convert.ToInt32(Math.Pow(10, length-i)));
+   
+        }
+       
+    }
+    return tempNumber;
+}
+
+int inputNumber = GetNumber("Введите число: ");
+int inputLength = GetLehgthNumber(inputNumber);
+int thirdNumber = GiveThirdNumber(inputNumber, inputLength);
+if(inputLength<=2) Console.Write("Третьей цифры нет");
+else 
+{Console.Write("Третья цифра числа ");
+Console.Write(inputNumber);
+Console.Write(" - ");
+Console.Write(thirdNumber);
+Console.Write(".");}
---
+++ Homework02/Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿// //  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int numberWeekDay = int.Parse(Console.ReadLine());
+
+if(numberWeekDay == 6 | numberWeekDay == 7) Console.WriteLine("Это выходной день");
+else Console.WriteLine("Это будний день");
