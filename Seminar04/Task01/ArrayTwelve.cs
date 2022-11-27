33ea544c3e17a12a35e7b4fcb6d46530fa52f230
Anka_S <anna.k.sidorova@gmail.com>
2022-11-27 20:43:05 +0300

    Задачи семинара 4

---
+++ Seminar04/Task01/ArrayTwelve.cs
@@ -0,0 +1,94 @@
+﻿using static MyLibrary;
+// Задачи на сегодня
+Console.Clear();
+
+// Задача 1: Задайте массив из 12 элементов, 
+// заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+
+// int[] col = CreateArray(12);
+// Console.WriteLine(PrintGood(col));
+// Fill(col, -9, 9);
+// Console.WriteLine(PrintGood(col));
+// int pos = SumPositive(col);
+// Console.WriteLine($">0 {pos}");
+// int neg = SumNegative(col);
+// Console.WriteLine($"<0 {neg}");
+
+
+// =====
+/* 
+int[] col3 = CreateArray(12);
+Fill(col3, -9, 10);
+Console.WriteLine(PrintGood(col3));
+int sumPos = SumPositive(col3);
+int sumNeg = SumNegative(col3);
+
+Console.WriteLine($"Сумма положительных элементов: {sumPos}");
+
+Console.WriteLine();
+
+Console.WriteLine($"Сумма отрицательных элементов: {sumNeg}");
+*/
+
+//==========
+// Задача 2: Напишите программу замена элементов
+// массива: положительные элементы замените на 
+// соответствующие отрицательные, и наоборот.
+// =====
+// int[] col2 = CreateArray(10);
+// Console.WriteLine(PrintGood(col2));
+// Fill(col2, -20, 20);
+// Console.WriteLine(PrintGood(col2));
+
+// int[] res = Inverse1(col2);
+// Inverse2(col2);
+// Console.WriteLine(PrintGood(res));
+// Console.WriteLine(PrintGood(col2));
+// =====
+
+
+// Задача 3: Задайте массив.
+// Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+
+//int upperBound = 50;
+//int searchElement = Random.Shared.Next(0, upperBound);
+
+
+
+//bool flag1 = FindElement1(col3, searchElement);
+//Console.WriteLine($"{searchElement} найден - {flag1}");
+
+//bool flag2 = FindElement2(col3, searchElement);
+//Console.WriteLine($"{searchElement} найден - {flag2}");
+
+
+// =====
+// Задача 4: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке[10, 99]. 
+
+/*
+int[] array4 = CreateArray(123);
+Fill(array4, 0, 200);
+Console.WriteLine(PrintGood(array4));
+Console.WriteLine(CountElement(array4));
+*/
+
+// Задача 5: Найдите произведение пар чисел в одномерном массиве.Парой считаем первый и последний элемент, второй и предпоследний и т.д.Результат запишите в новом массиве.
+// * Для нечетного количество центральный элемент возводится в квадрат
+
+int[] array4 = CreateArray(10);
+Fill(array4, 0, 10);
+Console.WriteLine(PrintGood(array4));
+int sizeArray = array4.Length/2;
+if (sizeArray % 2 ==0)
+{
+    int[] array5 = MultElementEven(array4);
+}
+else
+{
+    int[] array5 = MultElementOdd(array4);
+}
+
+
+Console.WriteLine(PrintGood(array5));
---
+++ Seminar04/Task01/MyLibrary.cs
@@ -0,0 +1,153 @@
+ public static class MyLibrary
+{
+  // печать массив
+  public static string PrintGood(int[] numbers)
+  {
+    int size = numbers.Length;
+    int i = 0;
+    string result = "[ ";
+
+    while (i < size)
+    {
+      result += ($"{numbers[i],3} ");
+      i++;
+    }
+    return result + "]";
+  }
+
+  // метод, который создаёт массив
+  public static int[] CreateArray(int count)
+  {
+    return new int[count];
+  }
+
+  // заполняет массив
+  public static void Fill(int[] array, int min, int max)
+  {
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = Random.Shared.Next(min, max + 1);
+      //array[i] = new Random().Next(min, max);
+    }
+  }
+
+  public static int SumPositive(int[] numbers)
+  {
+    int result = 0;
+    int size = numbers.Length;
+    for (int i = 0; i < size; i++)
+    {
+      if (numbers[i] > 0) result += numbers[i];
+    }
+    return result;
+  }
+
+  public static int SumNegative(int[] numbers)
+  {
+    int result = 0;
+    int size = numbers.Length;
+    for (int i = 0; i < size; i++)
+    {
+      if (numbers[i] < 0) result += numbers[i];
+    }
+    return result;
+  }
+
+
+  public static int[] Inverse1(int[] input)
+  {
+    int size = input.Length;
+    int[] output = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+      output[i] = -input[i];
+    }
+    return output;
+  }
+
+  public static void Inverse2(int[] input)
+  {
+    int size = input.Length;
+    for (int i = 0; i < size; i++)
+    {
+      input[i] = -input[i];
+    }
+  }
+
+  public static bool FindElement1(int[] col, int find)
+  {
+    int size = col.Length;
+    for (int i = 0; i < size; i++)
+    {
+      if (col[i] == find) return true;
+    }
+    return false;
+  }
+
+  public static bool FindElement2(int[] col, int find)
+  {
+    // 0 1 2 3 4 5
+    // 1 5 1 2 3 9
+    // 4 <- -1
+    // 1 <- 0
+    // 5 <- 1
+
+    return Array.IndexOf(col, find) != -1;
+  }
+
+// метод, который возвращает количество элементов массива в интервале от 10 до 100
+
+public static int CountElement(int[] col)
+  {
+    int size = col.Length;
+    int sumEl = 0;
+    for (int i = 0; i < size; i++)
+    {
+      if (col[i] >= 10 & col[i]<=100)
+      {
+          sumEl += 1; 
+      }  
+    }
+    return sumEl;
+  }
+
+// метод, который возвращает произведение пар чисел в одномерном массиве.Парой считаем первый и последний элемент, второй и предпоследний и т.д.Результат запишите в новом массиве.
+// * Для нечетного количество центральный элемент возводится в квадрат
+
+public static int[] MultElementEven(int[] col)
+  {
+    int size = col.Length;
+    int c = size/2;
+    int j = 0;
+    int [] resArray = new int[c];
+   
+        for (int i = 0; i < c; i++)
+        {
+          resArray[j] = col[i] * col[(size-1)-i];
+          j = j+1;
+        }
+   
+    return resArray;
+  }
+
+public static int[] MultElementOdd(int[] col)
+  {
+    int size = col.Length;
+    int c = size/2+1;
+    int j = 0;
+    int i = 0;
+    int [] resArray = new int[c];
+
+        for (i = 0; i < c-1; i++)
+        {
+          resArray[j] = col[i] * col[(size-1)-i];
+          j = j+1;
+        }
+        resArray[j+1] = col[i+1]*col[i+1];
+   return resArray;
+  
+  }
+    
+  
+}
