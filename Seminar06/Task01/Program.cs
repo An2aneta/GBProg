a5513a0da56a2d3fc2858df60309325e5e784e42
Anka_S <anna.k.sidorova@gmail.com>
2022-11-28 23:51:56 +0300

    Сложение/вычитание многочленов

---
+++ Seminar06/Task01/Program.cs
@@ -0,0 +1,138 @@
+﻿int[] CreateArray(int count)
+  {
+    return new int[count];
+  }
+
+  // заполняет массив
+void Fill(int[] array, int min, int max)
+  {
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = Random.Shared.Next(min, max + 1);
+      //array[i] = new Random().Next(min, max);
+    }
+  }
+
+  int[] NewArraySum(int[] array1, int[] array2, int degreemax, int degreemin)
+  {
+     
+    int[] resultArray = new int[degreemax]; 
+     
+     for(int i = 0; i< degreemin; i++)
+    {
+       resultArray[i] = array1[i] + array2[i];
+    }
+
+        if(array1.Length > array2.Length)
+        {
+            for(int i = degreemin; i< degreemax; i++)
+                 {
+                     resultArray[i] = array1[i];
+                 }
+        }
+        else
+        {
+            for(int i = degreemin; i< degreemax; i++)
+                 {
+                     resultArray[i] = array2[i];
+                 }
+        }
+     
+    return resultArray;
+  }
+
+int[] NewArraySub(int[] array1, int[] array2, int degreemax, int degreemin)
+  {
+     
+    int[] resultArray = new int[degreemax]; 
+     
+     for(int i = 0; i< degreemin; i++)
+    {
+       resultArray[i] = array1[i] - array2[i];
+    }
+
+        if(array1.Length > array2.Length)
+        {
+            for(int i = degreemin; i< degreemax; i++)
+                 {
+                     resultArray[i] = array1[i];
+                 }
+        }
+        else
+        {
+            for(int i = degreemin; i< degreemax; i++)
+                 {
+                     resultArray[i] = array2[i];
+                 }
+        }
+     
+    return resultArray;
+  }
+
+
+string PrintGood(int[] numbers)
+  {
+    int size = numbers.Length;
+    int i = 0;
+    string result = "[ ";
+
+    while (i < size)
+    {
+      result += ($"{numbers[i],3} ");
+      i++;
+    }
+    return result + "]";
+  }
+
+
+  string Printres(int[] resArr)
+  {
+    string output = String.Empty;
+    for(int i = 0; i < resArr.Length; i++)
+    {
+        int t = resArr[i];
+        if (t == 0) continue;
+        if (t < 0) {output += " - "; }
+        else if (i != 0) {output += " - "; }
+
+        if (t < 0) t = -t;
+        if (i == 1) {output += $"{t}x"; }
+        if (i == 0) {output += $"{t}"; }
+        if (i != 1 && i != 0 && t != 0) {output += $"{t}x^{i}"; }
+    }
+        return output;
+  }
+
+  Console.WriteLine("Введите степень первого многочлена: ");
+  int n1 = Convert.ToInt32(Console.ReadLine()) + 1;
+  int[] firstArray = CreateArray(n1);
+  Console.WriteLine("Введите степень второго многочлена: ");
+  int n2 = Convert.ToInt32(Console.ReadLine()) + 1;
+  int[] secondArray = CreateArray(n2);
+  Fill(firstArray, -10, 10);
+  Fill(secondArray, -10, 10);
+
+  int maxDegree = n1;
+  int minDegree = n2; 
+  if (n2 > n1)
+  {
+    maxDegree = n2;
+    minDegree = n1;
+  } 
+  
+  int[] sumArray =  NewArraySum(firstArray, secondArray, maxDegree, minDegree);
+  int[] subArray =  NewArraySub(firstArray, secondArray, maxDegree, minDegree);
+
+  Console.WriteLine(PrintGood(firstArray));
+  Console.WriteLine(PrintGood(secondArray));
+  Console.WriteLine(PrintGood(sumArray));
+  Console.WriteLine();
+  Console.WriteLine(PrintGood(subArray));
+
+  Console.WriteLine(Printres(sumArray));
+  
+
+
+  
+
