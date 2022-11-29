3e78ef6e332732f35268b53efcb4d55c2fd0a965
Anka_S <anna.k.sidorova@gmail.com>
2022-11-29 21:37:28 +0300

    Seminar 7 start

--- Seminar07/Task01/Seminarsevenfirst.cs
+++ Seminar07/Task01/Seminarsevenfirst.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+void PrintTriangle(int[,] triangle)
+{
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < row; j++)
+    {
+      if (triangle[i, j] != 0) Console.Write($"{triangle[i, j],4}");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[] GetKoeff(int[,] tr, int pow)
+{
+  int[] row = new int[pow + 1];
+  for (int i = 0; i <= pow; i++)
+  {
+    row[i] = tr[pow, i];
+  }
+  return row;
+}
+
+int[,] tr = CreateTriangle(7);
+PrintTriangle(tr);
+int[] koeff = GetKoeff(tr, 5);
+
+System.Console.WriteLine(String.Join(' ', koeff));
+
+for (int i = 0; i < koeff.Length; i++)
+{
+  Console.Write($"{koeff[i]}*a^{koeff.Length - i}*b^{i} + ");
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
