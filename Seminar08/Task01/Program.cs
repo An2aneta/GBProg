afa24670f0b882a8566b6ee5b2be8b339e19d4c5
Anka_S <anna.k.sidorova@gmail.com>
2022-12-01 22:03:32 +0300

    двумерные массивы

---
+++ Seminar08/Task01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Показать двумерный массив, заполненный целыми числами
+
+// Метод создания массива
+
+int[,]  CreateTwoDimArray(int m, int n)
+{
+    return new int[m, n];
+}
+
+
+// Метод заполнения массива
+void FillTwoDimArray(int[,] array, int min, int max)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Random.Shared.Next(min, max + 1);
+        }
+    }
+}
+
+
+//Метод печати массива
+void PrintTwoDimArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i, j],4}");
+    }
+    Console.WriteLine();
+  }
+}
+
+int [,] matrix = CreateTwoDimArray(5, 7);
+FillTwoDimArray(matrix, -10, 10);
+PrintTwoDimArray(matrix);
