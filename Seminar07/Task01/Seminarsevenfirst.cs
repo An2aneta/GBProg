3a519a551e8223b95eeee44be753d57a050e4302
Anka_S <anna.k.sidorova@gmail.com>
2022-12-03 22:57:15 +0300

    Seminar 7 done

--- Seminar07/Task01/Seminarsevenfirst.cs
+++ Seminar07/Task01/Seminarsevenfirst.cs
@@ -1,4 +1,6 @@
-﻿int[,] CreateTriangle(int row)
+﻿
+// метод для создания треугольника Паскаля для небольших степеней
+int[,] CreateTriangle(int row)
 {
   int[,] triangle = new int[row, row];
   for (int i = 0; i < row; i++)
@@ -18,6 +20,28 @@
   return triangle;
 }
 
+//метод для создания треугольника Паскаля для больших степеней 
+double[,] CreateTriangleDouble(int row)
+{
+  double[,] triangleDouble = new double[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangleDouble[i, 0] = 1;
+    triangleDouble[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangleDouble[i, j] =
+      triangleDouble[i - 1, j - 1] + triangleDouble[i - 1, j];
+    }
+  }
+  return triangleDouble;
+}
+
+//метод для вывода треугольника Паскаля для небольших степеней
 void PrintTriangle(int[,] triangle)
 {
   int row = triangle.GetLength(0);
@@ -31,6 +55,23 @@ void PrintTriangle(int[,] triangle)
   }
 }
 
+//метод для вывода треугольника Паскаля для больших степеней, печать для нечетных элементов, замененных на символ "@" 
+void PrintTriangleDouble(double[,] triangleDouble)
+{
+  int row = triangleDouble.GetLength(0);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < row; j++)
+    {
+      if (triangleDouble[i, j] != 0 && triangleDouble[i, j]%2 == 0) Console.Write("   ");
+      if (triangleDouble[i, j] != 0 && triangleDouble[i, j]%2 != 0) Console.Write(" @ ");
+    }
+
+    Console.WriteLine();
+  }
+}
+
+//метод для получения массива коэффициентов для заданной степени (небольшой)
 int[] GetKoeff(int[,] tr, int pow)
 {
   int[] row = new int[pow + 1];
@@ -41,19 +82,61 @@ int[] GetKoeff(int[,] tr, int pow)
   return row;
 }
 
-int[,] tr = CreateTriangle(7);
+//метод для получения массива коэффициентов для заданной степени (большой)
+double[] GetKoeffDouble(double[,] tr, int pow)
+{
+  double[] row = new double[pow + 1];
+  for (int i = 0; i <= pow; i++)
+  {
+    row[i] = tr[pow, i];
+  }
+  return row;
+}
+
+//вывод многочлена
+string Printres(int[] resArr)
+  {
+    string output = String.Empty;
+    for(int i = 0; i < resArr.Length; i++)
+    {
+        int t = resArr[i];
+         if (t == 1 && i == 0)
+          {
+            output += $"a^{(resArr.Length-1)} + ";
+          } 
+         else if(i==1)
+         {
+            output += $"{resArr[i]}*a^{resArr.Length - 1 -i}*b + ";
+         } 
+         else if (t != 1 && i !=0 && i != resArr.Length-1 && i != 1 && i != resArr.Length-2)
+         {
+           output += $"{resArr[i]}*a^{resArr.Length - 1 - i}*b^{i} + ";
+         }
+         else if(i == resArr.Length-2)
+         {
+           output += $"{resArr[i]}*a*b^{i} + ";
+         } 
+         else if (t == 1 && i == resArr.Length-1)
+         {
+          output += $"b^{i}"; 
+         }
+    }
+        return output;
+  }
+
+
+/* int[,] tr = CreateTriangle(10);
 PrintTriangle(tr);
-int[] koeff = GetKoeff(tr, 5);
+int[] koeff = GetKoeff(tr, 8);
 
 System.Console.WriteLine(String.Join(' ', koeff));
 
-for (int i = 0; i < koeff.Length; i++)
-{
-  Console.Write($"{koeff[i]}*a^{koeff.Length - i}*b^{i} + ");
-}
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
+Console.WriteLine(Printres(koeff));
+*/
+
+
+double[,] tr = CreateTriangleDouble(125);
+double[] koeff = GetKoeffDouble(tr, 123);
+Console.WriteLine(String.Join(' ', koeff));
+
+PrintTriangleDouble(tr);
--- Seminar08/Task01/Program.cs
+++ Seminar08/Task01/Program.cs
@@ -34,6 +34,20 @@ void PrintTwoDimArray(int[,] array)
   }
 }
 
+string PrintTwoDimArrayGood(int[,] array)
+{
+  string result = string.Empty;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      result += $"{array[i, j],4};
+    }
+    result += "\n";
+  }
+  return result;
+}
+
 int [,] matrix = CreateTwoDimArray(5, 7);
 FillTwoDimArray(matrix, -10, 10);
 PrintTwoDimArray(matrix);
