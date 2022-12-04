e8f6b98602653926140be4a4bd2dd11addbc0449
Anka_S <anna.k.sidorova@gmail.com>
2022-12-04 17:44:26 +0300

    Homework09 second task

---
+++ Homework09/Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumEl(int m, int n)
+{
+    int sum = n;
+    if(m != n)
+    {
+        sum = sum + SumEl(m, n - 1);
+    }
+    return sum;
+}
+
+Console.WriteLine(SumEl(2, 8));
